01ffb4712d238c34a62d59bdc9bb066f598ecf3c
tophat8855 <hrfarley@gmail.com>
2015-02-11 15:39:00 -0800

    practice nesting resources

--- javascripts/router.js
+++ javascripts/router.js
@@ -1,17 +1,33 @@
 App.Router.map(function() {
-  this.route("about");
   this.route("personal");
   this.route("connect");
-  this.route("experience", {path: 'about/:experience_id'});
+
+  this.resource('about', function() {
+    this.route('experience', {path: '/:experience_id'});
+  });
+
 });
 
 App.AboutRoute = Ember.Route.extend({
   model: function() {
     return [
-      {id: 1, description: "Fussbudget", link: "http://www.peanuts.com/comicstrips/3259085"},
-      {id: 2, description: "Own Psychiatric Practice for 3 Years", link: "http://www.peanuts.com/comicstrips/3286446"},
-      {id: 3, description: "Role of Christmas Queen in Christmas Play", link: "https://www.youtube.com/watch?v=-NJGRiV8jGM"},
-      {id: 4, description: "Right Fielder for local baseball team", link: "http://www.peanuts.com/comicstrips/3259297"},
+      {id: 1, description: "Fussbudget", img: "images/fuss.jpg"},
+      {id: 2, description: "Own Psychiatric Practice for 3 Years", img: "images/psych.jpg"},
+      {id: 3, description: "Role of Christmas Queen in Christmas Play", img: "images/queen.jpg"},
+      {id: 4, description: "Right Fielder for local baseball team", img: "images/baseball.jpg"},
+      {id: 5, description: "Collected Halloween candy for kids trapped in delusions of the Great Pumpkin", img: "images/halloween.jpg"},
+      {id: 6, description: "Assists local kids in football punting practice", img: "images/football.jpg"},
+    ];
+  }
+});
+
+App.PersonalRoute = Ember.Route.extend({
+  model: function() {
+    return[
+    {id: 1, description: "Classical Musicians", link: "http://www.peanuts.com/comicstrips/3256867"},
+    {id: 2, description: "Warm Puppies", link: "http://www.peanuts.com/comicstrips/3256711"},
+    {id: 3, description: "Crashing busts of Beethoven", link: "http://vignette2.wikia.nocookie.net/peanuts/images/f/fd/Pe550227.gif/revision/latest?cb=20130709010950"},
+
     ];
   }
 });
