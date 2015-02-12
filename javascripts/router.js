3410e98c5aead34063070f79d19461b9096811d0
tophat8855 <hrfarley@gmail.com>
2015-02-11 21:36:34 -0800

    got ember-data and models to work

--- javascripts/router.js
+++ javascripts/router.js
@@ -13,14 +13,7 @@ App.Router.map(function() {
 
 App.AboutRoute = Ember.Route.extend({
   model: function() {
-    return [
-      {id: 1, description: "Fussbudget", img: "images/fuss.jpg"},
-      {id: 2, description: "Own Psychiatric Practice for 3 Years", img: "images/psych.jpg"},
-      {id: 3, description: "Role of Christmas Queen in Christmas Play", img: "images/queen.jpg"},
-      {id: 4, description: "Right Fielder for local baseball team", img: "images/baseball.jpg"},
-      {id: 5, description: "Collected Halloween candy for kids trapped in delusions of the Great Pumpkin", img: "images/halloween.jpg"},
-      {id: 6, description: "Assists local kids in football punting practice", img: "images/football.jpg"},
-    ];
+    return this.store.find('experience');
   }
 });
 
